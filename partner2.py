951d3a002ef378414ce9319eaf964e381e78f8da
WolfOPixels29 <evan.sanchez01@utrgv.edu>
2026-02-19 05:12:13 +0000

    completed in class

--- partner2.py
+++ partner2.py
@@ -1,31 +1,34 @@
 # 1. Print a message to the screen 
-Hello
+from os import name, nice
+from tkinter import Variable
+
+
+Hello # type: ignore
 # 2. Change the message to your own words and run again 
-Hello world
+Hello # type: ignore
 # 3. Print your favorite food 
-Print (pizza)
+print (pizza) # type: ignore
 # 4. Try printing a number instead of text 
-Print (nine)
+print (nice)
 # 5. Create a variable that stores your name 
-variable (Evan)
+variable (Evan) # type: ignore
 # 6. Print the variable 
-print (Evan)
+print (Evan) # type: ignore
 # 7. Change the name inside the variable and run again 
-Change (Evan)
+Change (Evan) # type: ignore
 num=9
 # 8. Create a variable with a number 
-variable (nine)
+Variable (nice)
 # 9. Print the age 
-Print(22)
+print(22)
 # 10. Add 2 to the age and print the result
-Print (24)
+print (24)
 # 11. Ask the user for their name input 
-name input ("what is your name")
+name ;input ("what is your name")
 # 12. Print a greeting using their name
 print ("my name is Evan")
 # 13. Ask the user for a number 
 ("give me a number")
 # 14. Print the number they typed
-(nine)
-# 15. Print a goodbye message
-Print (farewell)
+n() 15. Print a goodbye message # type: ignore
+print ()
